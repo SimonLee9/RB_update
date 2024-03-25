f01e19d91af67d13d7d992fb327fe704b3dd0d11
Beomjin Lee <74641464+SimonLee9@users.noreply.github.com>
2024-03-25 15:01:27 +0900

    Add files via upload

---
+++ srv_update2.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo apt update
+
+sudo apt install qml-module-qtquick-* qml-module-qtmultimedia qml-module-qt-labs-platform libqt5serialport5-dev qtquickcontrols2-5-dev ccache libusb-1.0-0-dev libqt5multimedia5-plugins -y libqt5websockets5-dev -y
+
+sudo apt install libquazip5-dev -y
+
+sudo apt install python3-pip -y
+
+pip3 install gtts
+
+sudo apt install gstreamer1.0-libav gstreamer1.0-vaapi ubuntu-restricted-extras -y
+
