75fb2cfa4b39b1af5f47687883d5a450ebf78eca
Beomjin Lee <74641464+SimonLee9@users.noreply.github.com>
2024-03-25 15:03:07 +0900

    Update srv_update2.sh

--- srv_update2.sh
+++ srv_update2.sh
@@ -2,7 +2,7 @@
 
 sudo apt update
 
-sudo apt install qml-module-qtquick-* qml-module-qtmultimedia qml-module-qt-labs-platform libqt5serialport5-dev qtquickcontrols2-5-dev ccache libusb-1.0-0-dev libqt5multimedia5-plugins -y libqt5websockets5-dev -y
+sudo apt install qml-module-qtquick-* qml-module-qtmultimedia qml-module-qt-labs-platform libqt5serialport5-dev qtquickcontrols2-5-dev ccache libusb-1.0-0-dev libqt5multimedia5-plugins -y libqt5websockets5-dev -y qtmultimedia5-dev -y
 
 sudo apt install libquazip5-dev -y
 
