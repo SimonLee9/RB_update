1a645eb428e7fb5a7d31cc93496b3831072ce029
Simon_RB <lbj142632@rainbow-robotics.com>
2024-03-25 15:30:51 +0900

    install-qt

---
+++ install_qt.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# 필수 빌드 도구 설치
+sudo apt install build-essential make -y
+
+# Qt5 기본 라이브러리 설치
+sudo apt install qt5-default -y
+
+# QtCreator 설치
+sudo apt install qtcreator -y
+
+# Qt Declarative (Qt Quick) 개발 패키지 설치
+sudo apt install qtdeclarative5-dev -y
