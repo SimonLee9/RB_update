97e378b9bb370983cb7de50af709fbfe973695e8
SimonLee9 <lbj142632@gmail.com>
2024-04-01 22:39:35 +0900

    update

--- srv_update2.sh
+++ srv_update2.sh
@@ -4,6 +4,8 @@ sudo apt update
 
 sudo apt install qml-module-qtquick-* qml-module-qtmultimedia qml-module-qt-labs-platform libqt5serialport5-dev qtquickcontrols2-5-dev ccache libusb-1.0-0-dev libqt5multimedia5-plugins -y libqt5websockets5-dev -y qtmultimedia5-dev -y qtdeclarative5-dev -y
 
+sudo apt install build-essential make -y
+
 sudo apt install libquazip5-dev -y
 
 sudo apt install python3-pip -y
