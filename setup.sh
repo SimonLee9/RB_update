45ecc0b9181e9096551ef929167ab364f450d6d5
Beomjin Lee <74641464+SimonLee9@users.noreply.github.com>
2024-04-01 21:33:52 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,3 +5,21 @@ sudo apt-get update
 sudo apt-get upgrade
 
 sudo apt-get install gedit -y
+
+
+#Hangle
+sudo apt-get install fonts-nanum*
+
+sudo dpkg-reconfigure locales # change to ko_KR.UTF-8
+
+sudo apt install fcitx fcitx-hangul fonts-noto-cjk dbus-x11 # select, fcitx
+
+sudo apt install language-selector-gnome
+
+sudo gnome-language-selector
+
+sudo /etc/init.d/dbus start
+
+fcitx-config-gtk3
+
+
