a651856431f98e6b5650753ed4f6de906c84ba7e
SimonLee9 <lbj142632@gmail.com>
2024-05-31 09:04:56 +0900

    slam

---
+++ install-slam.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# https://docs.luxonis.com/projects/api/en/latest/install/
+sudo wget -qO- https://docs.luxonis.com/install_dependencies.sh | bash
+
+# c++ sdk install
+git clone https://github.com/luxonis/depthai-core.git
+cd depthai-core/
+git checkout v2.25.1
+git submodule update --init --recursive
+mkdir build
+cd build
+cmake ..
+cmake-gui ..
+
