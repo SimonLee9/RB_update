5cb2b8b4f4ad864eb708683d439663181ab61140
SimonLee9 <lbj142632@gmail.com>
2024-06-18 23:09:27 +0900

    update-srv

---
+++ update-srv.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt-get update
+
+sudo apt install libqt5websockets5-dev -y
+
+sudo apt-get install qtmultimedia5-dev
+
+sudo apt-get install libquazip5-dev
+
+sudo apt-get -y install sshpass
