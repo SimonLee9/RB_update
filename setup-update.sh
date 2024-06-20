8a5b0e8d53318ca714a9cb24760e891898c401d2
SimonLee9 <lbj142632@gmail.com>
2024-06-20 15:34:55 +0900

    full-update

--- setup-update.sh
+++ setup-update.sh
@@ -4,22 +4,23 @@ sudo apt-get update
 
 sudo apt install libqt5websockets5-dev -y
 
-sudo apt-get install qtmultimedia5-dev
+sudo apt-get install qtmultimedia5-dev -y
 
-sudo apt-get install libquazip5-dev
+sudo apt-get install libquazip5-dev -y
 
-sudo apt-get -y install sshpass
+sudo apt-get -y install sshpass -y
 
 sudo apt install qtdeclarative5-dev -y # vtk 빌드를 위한 QT5Qml, QT5Quick 설치
 
-sudo apt install libvtk9-qt-dev
+sudo apt install libvtk9-qt-dev -y
 
-sudo apt install libpcl-dev 
+sudo apt install libpcl-dev -y
 
-sudo apt install libeigen3-dev 
+sudo apt install libeigen3-dev -y
 
-sudo apt libopencv-dev libopencv-contrlib-dev
+sudo apt install libopencv-dev -y
+sudo apt install libopencv-contrib-dev -y
 
-sudo apt install libboost-all
+sudo apt install libboost-all-dev -y
 
 
