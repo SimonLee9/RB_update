c1df2411479261f8f450d846f08da9b3a4e5a5e3
SimonLee9 <lbj142632@gmail.com>
2024-06-27 11:26:11 +0900

    setupfile-dir-change

--- setup-teamviewer.sh
+++ setup-teamviewer.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-
+# 패키지 리스트 업데이트
 sudo apt-get update
 
+# 팀뷰어 설치 파일을 Home 경로에 다운로드
+wget -P ~/ https://download.teamviewer.com/download/linux/teamviewer_amd64.deb
 
-wget https://download.teamviewer.com/download/linux/teamviewer_amd64.deb
-
-sudo apt install ./teamviewer_amd64.deb -y
+# 다운로드한 설치 파일을 이용하여 팀뷰어 설치
+sudo apt install ~/teamviewer_amd64.deb -y
 
