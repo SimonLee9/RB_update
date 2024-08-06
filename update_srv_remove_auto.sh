224a6dec205f30459cedf0996d55fa7b200ab777
SimonLee9 <lbj142632@gmail.com>
2024-08-06 18:20:23 +0900

    update:Teamviewer

--- update_srv_remove_auto.sh
+++ update_srv_remove_auto.sh
@@ -32,7 +32,8 @@ sudo apt install nmap-common -y #0725
 
 # 팀뷰어 설치 파일을 Home 경로에 다운로드
 #wget -P ~/ https://download.teamviewer.com/download/linux/teamviewer_amd64.deb
-wget -P ~/ https://download.teamviewer.com/download/linux/teamviewer_arm64.deb
+#wget -P ~/ https://download.teamviewer.com/download/linux/teamviewer_arm64.deb
+wget -P ~/ https://download.teamviewer.com/download/linux/teamviewer-host_arm64.deb
 
 # 다운로드한 설치 파일을 이용하여 팀뷰어 설치
 #sudo apt install ~/teamviewer_amd64.deb -y
