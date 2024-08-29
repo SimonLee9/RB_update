084f92929ed9bd0b040bd948fb39168fa4955803
SimonLee9 <lbj142632@gmail.com>
2024-08-29 17:21:49 +0900

    teamviwer-host

--- setup-teamviewer-Host.sh
+++ setup-teamviewer-Host.sh
@@ -4,7 +4,7 @@
 sudo apt-get update
 
 # 팀뷰어 설치 파일을 Home 경로에 다운로드
-#wget -P ~/ https://download.teamviewer.com/download/linux/teamviewer_amd64.deb
+#wget -P ~/ https://download.teamviewer.com/download/linux/teamviewer_arm64.deb
 wget -P ~/ https://download.teamviewer.com/download/linux/teamviewer-host_arm64.deb
 
 # 다운로드한 설치 파일을 이용하여 팀뷰어 설치
